f33dcce6f3ae5f6c29b53d4bbdf71c4c43e60564
daniel.casmendoza@gmail.com <Daniel.Castillo@ibm.com>
2022-01-12 13:47:32 -0600

    index.html actualizado

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,19 +1,32 @@
-import React, { useEffect, useState } from "react";
-import { getGifs } from "../helpers/getGifs";
+import { useState, useEffect } from 'react'
+import { getGifs } from '../helpers/getGifs';
+
 
 export const useFetchGifs = ( category ) => {
+    
     const [state, setState] = useState({
         data: [],
-        loading: true,
+        loading: true
     });
 
-    useEffect(() => {
-        getGifs(category)
-            .then(imgs => setState({
-                data: imgs,
-                loading: false
-            }))
+    useEffect( () => {
+
+        getGifs( category )
+            .then( imgs => {
+                
+                setState({
+                    data: imgs,
+                    loading: false
+                });
+            })
+
     }, [category])
 
-    return state;
+
+
+
+    return state; // { data:[], loading: true };
+
+
 }
+

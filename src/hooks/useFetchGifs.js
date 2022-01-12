17c472c70ac48a0985c73fee536105e3b19436c7
daniel.casmendoza@gmail.com <Daniel.Castillo@ibm.com>
2022-01-12 13:23:46 -0600

    index.html actualizado

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,5 +1,4 @@
-import { useState } from "react"
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react"
 import { getGifs } from "../helpers/getGifs";
 
 export const useFetchGifs = ( category ) => {
